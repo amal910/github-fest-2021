2720314ab84279e0e965d5ad84f5aa5b61617f9b
fayiza96 <fayisafayi56@gmail.com>
2021-10-14 13:14:15 +0530

    values

--- Mine/values.dart
+++ Mine/values.dart
@@ -3,11 +3,16 @@ main() {
   print("dart" + "lang");
 
   // Integers and Floats
-  print("1+1=${1+1}");
-  print("7.0/3.0 =${7.0/3.0}");
+  print("1+1=${1 + 1}");
+  print("7.0/3.0 =${7.0 / 3.0}");
 
   // Booleans
+  print('\nboolean true && false :');
   print(true && false);
+
+  print('\nboolean true || false :');
   print(false || true);
+
+  print('\nboolean !true :');
   print(!true);
 }
